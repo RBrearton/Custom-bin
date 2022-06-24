92e3ba10b1b49ca4cbc915f446d0a7ad7308f528
Brearton, Richard (DLSLtd,RAL,LSCI) <richard.brearton@diamond.ac.uk>
2022-06-24 10:08:12 +0100

    Trying some different build flags to force C99

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@
 # encoding: utf-8
 
 from distutils.core import setup, Extension
+import sysconfig
+
+EXTRA_COMPILE_ARGS = sysconfig.get_config_var('CFLAGS').split()
+EXTRA_COMPILE_ARGS += ["-std=C99"]
 
 
 def configuration(parent_package='', top_path=None):
@@ -16,7 +20,9 @@ def configuration(parent_package='', top_path=None):
                            top_path)
     config.add_extension('mapper_c_utils',
                          ['src/mapper_c_utils/mapper_c_utils.c'],
-                         extra_info=info)
+                         extra_info=info,
+                         language='C99',
+                         extra_compile_args=EXTRA_COMPILE_ARGS)
 
     return config
 
