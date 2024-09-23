8511bc58ee020777c66da97afbf9b9ab54780ae2
P-Mousley <philipmousley@hotmail.com>
2024-09-23 17:03:34 +0100

    Revert "port setup.py from distutils and numpy.disutils  to setuptools"
    This reverts commit 24adb22ace088b5a2a09906caae675cba6b9bfde.

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from setuptools import Extension, setup, find_packages
+from distutils.core import setup, Extension
 import sysconfig
 
 EXTRA_COMPILE_ARGS = sysconfig.get_config_var('CFLAGS').split()
 EXTRA_COMPILE_ARGS += ["-std=c99"]
 
-ext_modules=[Extension('mapper_c_utils',
-                          ['src/mapper_c_utils/mapper_c_utils.c'],
-                          language='c99',
-                          extra_compile_args=EXTRA_COMPILE_ARGS)]
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    from numpy.distutils.misc_util import get_info
+
+    # Necessary for the half-float d-type.
+    info = get_info('npymath')
+
+    config = Configuration('',
+                           parent_package,
+                           top_path)
+    config.add_extension('mapper_c_utils',
+                         ['src/mapper_c_utils/mapper_c_utils.c'],
+                         extra_info=info,
+                         language='c99',
+                         extra_compile_args=EXTRA_COMPILE_ARGS)
+
+    return config
+
 
 if __name__ == "__main__":
-    setup(name='mapper_c_utils',
-          version='0.1',
-          packages=find_packages(),
-          ext_modules=ext_modules,
-          
-        )
+    from numpy.distutils.core import setup
+    setup(configuration=configuration)
